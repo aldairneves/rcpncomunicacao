aef7372644f7cca0aedf2f71c870a2842ccccb8b
aldair <aldairneves608@gmail.com>
2023-03-12 20:34:05 -0300

    criando telas e ajustando

--- js/imprimir.js
+++ js/imprimir.js
@@ -1,39 +1,3 @@
-
-/* inserir input */
-
-var hoje = document.getElementById('data');
-var nomesNub = document.getElementById('nomesNubentes');
-var insereData = document.getElementById('dataRegistro');
-var livro = document.getElementById('numeroLivro');
-var serventi = document.getElementById('serventia');
-var nubNomealt = document.getElementById('nomeAlterado');
-
-function insere() {
-    var data = hoje.value;
-    if (data == '') data = '';
-    dataHoje.innerHTML = data;
-
-    var nomesNubentes = nomesNub.value;
-    if (nomesNubentes == '') nomesNubentes = '';
-    nomes.innerHTML = nomesNubentes;
-
-    var dataRegistro = insereData.value;
-    if (dataRegistro == '') dataRegistro = '';
-    dateManual.innerHTML = dataRegistro;
-
-    var numeroLivro = livro.value;
-    if (numeroLivro == '') numeroLivro = '';
-    numerosLFT.innerHTML = numeroLivro;
-
-    var serventia = serventi.value;
-    if (serventia == '') serventia = '';
-    cartorio.innerHTML = serventia;
-
-    var nomeAlterado = nubNomealt.value;
-    if (nomeAlterado == '') nomeAlterado = '';
-    nomeAl.innerHTML = nomeAlterado;
-}
-
 /* imprimir etiqueta */
 document.getElementById('btn').onclick = function () {
     var conteudo = document.getElementById('imprimir').innerHTML,
