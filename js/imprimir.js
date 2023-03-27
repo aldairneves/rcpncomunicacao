60d75a0bf2786f883cb77d27dbf08c83948820b1
aldair <aldairneves608@gmail.com>
2023-03-27 13:27:09 -0300

    conf impressao

--- js/imprimir.js
+++ js/imprimir.js
@@ -1,9 +1,31 @@
 /* imprimir etiqueta */
-document.getElementById('btn').onclick = function () {
+/* document.getElementById('btn').onclick = function () {
     var conteudo = document.getElementById('imprimir').innerHTML,
         tela_impressao = window.open('about:blank');
 
     tela_impressao.document.write(conteudo);
     tela_impressao.window.print();
     tela_impressao.window.close();
-}
+} */
+
+const btn_imp=document.getElementById("btn")
+
+btn_imp.addEventListener("click",(evt)=>{
+    const conteudo = document.getElementById("imprimir").innerHTML
+
+    let estilo = "<style>"
+    estilo += "p {text-align: justify; font-family: 'Roboto', sans-serif; font-size: 12px;}"
+    estilo += "</style>"
+
+    const win = window.open('about:blank');
+
+    win.document.write('<html><head>')
+    win.document.write('<title> Etiqueta</title>')
+    win.document.write(estilo)
+    win.document.write('</head><body>')
+    win.document.write(conteudo)
+    win.document.write('</body></html>')
+
+    win.print()
+    win.close()
+})
